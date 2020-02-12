381af0f2ded5b0102cb43edf34d1d28e6796d9d2
Yubhrraj Pathak <yubhrraj.p@gmail.com>
2020-02-12 11:18:31 +0530

    commit

---
+++ android/src/com/pathaboi/flabbybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pathaboi.flabbybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pathaboi.flabbybird.FlabbyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlabbyBird(), config);
+	}
+}
